7bc4beef718d035f9f3d7818d6bfb6e02e291ccb
Furkan Bilgin <info@furkanbilgin.net>
2026-02-25 12:26:58 +0300

    fix: fix migration

--- drizzle/0004_early_swordsman.sql
+++ drizzle/0004_early_swordsman.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "runs" ADD COLUMN "is_deleted" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "templates" ADD COLUMN "restart_on_crash" boolean DEFAULT true NOT NULL;
+ALTER TABLE "runs" ADD COLUMN "is_deleted" boolean DEFAULT false NOT NULL;
